c132945985968bd116cf80b902bca5dd7bbafb82
helderrl@yahoo.com.br <helderrl@yahoo.com.br>
2020-04-04 00:09:31 -0300

    Criado metodo atributo complexo

--- PythonPro_Cursos/pythonbirds-simples/oo/pessoa.py
+++ PythonPro_Cursos/pythonbirds-simples/oo/pessoa.py
@@ -1,13 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None):
+    def __init__(self, nome=None, idade=35, *filhos):
+
         self.nome = nome
+        self.idade=idade
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'ola {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa('Rogerio')
+    p=Pessoa('Rogerio')
+    guilherme = Pessoa(nome='Guilherme')
+    rogerio = Pessoa(guilherme, nome='Rogerio')
     print(id(p))
     print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
     print(p.nome)
     p.nome = 'Rogerio'
     print(p.nome)
+
+    for filho in rogerio.filhos:
+        print(filho.nome)
