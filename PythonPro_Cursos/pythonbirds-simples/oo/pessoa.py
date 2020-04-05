8491ab7d907441d4d4702d6423a9742f5cd39cc8
helderrl@yahoo.com.br <helderrl@yahoo.com.br>
2020-04-05 00:01:25 -0300

    Exercicio : colocando empratica os conceitos estudados ate aqui, trupoas, dicionarios, tipos, classes, variaveis, constantes, listas entre outros...

--- PythonPro_Cursos/pythonbirds-simples/oo/pessoa.py
+++ PythonPro_Cursos/pythonbirds-simples/oo/pessoa.py
@@ -1,22 +1,25 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35, *filhos):
+    olhos =2
+
+    def __init__(self, *filhos, nome=None, idade=35):
 
         self.nome = nome
-        self.idade=idade
+        self.idade = idade
         self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'ola {id(self)}'
 if __name__ == '__main__':
-    p=Pessoa('Rogerio')
     guilherme = Pessoa(nome='Guilherme')
     rogerio = Pessoa(guilherme, nome='Rogerio')
-    print(id(p))
-    print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Rogerio'
-    print(p.nome)
+    print(id(rogerio))
+    print(Pessoa.cumprimentar(rogerio))
+    print(rogerio.cumprimentar())
+    print(rogerio.nome)
+    rogerio.nome = 'Rogerio'
+    print(rogerio.nome)
 
     for filho in rogerio.filhos:
         print(filho.nome)
+
+   
